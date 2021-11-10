d2670aa11bff7b771faea7a9dcaf8519cf04fb18
Will Darkins <willdarkins@gmail.com>
2021-11-09 20:13:34 -0800

    post_id column added for vote model with reference to post model - key id

--- models/Vote.js
+++ models/Vote.js
@@ -1,7 +1,7 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Vote extends Model {}
+class Vote extends Model { }
 
 Vote.init(
     {
@@ -17,16 +17,23 @@ Vote.init(
                 model: 'user',
                 key: 'id'
             }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
         }
     },
-    
     {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'vote'
-      }
+    }
 );
 
 module.exports = Vote;
