ebbc96b70dc8720cabf25f21e03955ed09fd3c74
Will Darkins <willdarkins@gmail.com>
2021-11-10 19:27:21 -0800

    added GET route for finding all comments and POST route for creating comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 const withAuth = require('../../utils/auth');
 const { Comment } = require('../../models');
 
+//GET route - Find all comments 
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err=> {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
